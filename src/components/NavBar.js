1cbc4b8adae72a667d39478dfc6e5f77eed1feec
AdolfoCima <99370816+AdolfoCima@users.noreply.github.com>
2022-06-17 16:31:21 -0300

    creo el NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,52 @@
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        GAMER STORE
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarText"
+                        aria-controls="navbarText"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">
+                                    Inicio
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">
+                                    Novedades
+                                </a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Productos
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a className="dropdown-item" href="#">Teclados</a></li>
+                                    <li><a className="dropdown-item" href="#">Monitores</a></li>
+                                    <li><a className="dropdown-item" href="#">Mouses</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
